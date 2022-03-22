297d3c0c4af2e2dc79cafccd4d98630aa71ef656
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-03-22 20:59:37 +0530

    db in progress

---
+++ src/Components/Data.js
@@ -0,0 +1,20 @@
+const Userdata = [
+    {
+        username: 'User1',
+        password: 'Password1'
+    },
+    {
+        username: 'User2',
+        password: 'Password2'
+    },
+    {
+        username: 'User3',
+        password: 'Password3'
+    },
+    {
+        username: 'User4',
+        password: 'Password4'
+    },
+]
+
+export default Userdata
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginCss from "./Login.css";
 import { Card, Container, Form, Button } from "react-bootstrap";
 import Logo from "./assets/kriya.PNG";
 import Image1 from "./assets/img1.png";
+import Data from "./Data"
+import Dashboard from "./Dashboard"
 
 const Login = () => {
+  const [uname,setusername]=useState('')
+  const [upass,setPassword]=useState('')
+
+  
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    
+    console.log(uname,upass)
+    
+    
+  }
   return (
     <div className="login-page">
       <Container>
@@ -19,14 +33,14 @@ const Login = () => {
                 <h2 className="title">Welcome to KRiYA Kreations LLP</h2>
                 <Form>
                   <Form.Group className="mb-3 inputName" controlId="formBasicEmail" >
-                    <Form.Control type="email" placeholder="Username" />
+                    <Form.Control type="email" placeholder="Username" onChange={(e)=>setusername(e.target.value)} value={uname}/>
                   </Form.Group>
 
                   <Form.Group className="mb-3 inputPassword" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} value={upass}/>
                   </Form.Group>
 
-                  <Button variant="primary" type="submit" className="login-btn mb-3">
+                  <Button variant="primary" type="submit" className="login-btn mb-3" onClick={handleSubmit}>
                     Login
                   </Button>
                 </Form>
